afba341ef7611090cfd89e6a0011ed3d770b2270
Adel <adel.ferchichi93@gmail.com>
2022-06-30 11:42:51 +0100

    My first page home

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php 
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController {
+
+    /**
+     * @Route("/")
+     */
+    public function index(): Response
+    {
+        // return new Response('<html><body>Hello </body></html>');
+        return $this->render('Front/index.html.twig');
+    }
+
+}
